1ad5066d91afd04cbbd8ca56f508c987832ee1f1
dam2 <dam2@a223e16.dc.domenicoscarlatti.es>
2024-10-16 16:36:11 +0200

    Inicio Proyecto - Label con el título.

---
+++ src/space_invasion_dani/Space_Invasion_Dani.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package space_invasion_dani;
+
+/**
+ *
+ * @author dam2
+ */
+public class Space_Invasion_Dani {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
